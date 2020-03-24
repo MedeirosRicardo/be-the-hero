c6e1248c71f1578d4ee7e6593676aee877dbcf55
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-24 09:24:33 -0400

    Database setup

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
-const app = express();
+const routes = express.Router();
 
-app.get('/', (req, res) => {
+routes.get('/users', (req, res) => {
     return res.json({
         event: 'Semana OmniStack 11.0',
         user: 'Ricardo Medeiros'
     });
 });
-
-app.listen(3333);
